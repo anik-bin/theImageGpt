86ac42a82f8b51452a8213d472067641e7eccc72
Aniket Bindhani <aniketbindhani44@gmail.com>
2023-11-27 21:45:37 +0530

    Completed the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,80 +1,26 @@
-"use client";
-import Image from 'next/image'
-import { useState } from 'react';
-import axios from 'axios';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
+import Image from 'next/image';
+import Link from 'next/link';
+import homepageImage from "@/app/assets/homepage-image.jpeg"
+import { Button } from '@/components/ui/button';
 export default function Home() {
 
-  const [inputPrompt, setInputPrompt] = useState({
-    prompt: ""
-  });
-
-  const [loading, setLoading] = useState(false);
-
-  const [photo, setPhoto] = useState<string[]>([]);
-
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-
-    try {
-      setLoading(true);
-      setPhoto([]);
-      const response = await axios.post("/api/users/generate", inputPrompt);
-
-      const urls = response.data.map((image: { url: string }) => image.url);
-      setPhoto(urls);
-    } catch (error: any) {
-      console.error(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }
   return (
     <>
-      <div className='bg-white'>
-        <main className='flex flex-col gap-8 justify-center items-center pt-10'>
-          <h1 className='text-4xl text-black font-bold'>Generate Image</h1>
-
-          <form onSubmit={handleSubmit} className='flex gap-4'>
-            <input
-              className='p-4 border-none rounded-lg mb-4 text-black shadow-lg'
-              type="text"
-              value={inputPrompt.prompt}
-              placeholder='type something here'
-              onChange={(e) => {
-                setInputPrompt({ ...inputPrompt, prompt: e.target.value })
-              }}
-            />
-
-            <button type="submit" className='p-2 border bg-gray-300 border-gray-300 rounded-lg mb-4'>Generate</button>
-          </form>
-
-          <h1>{loading ? "Image is generating" : "Image is generated"}</h1>
-          <div>
-            {photo.map((src) => (
-              <Card key={src}>
-                <CardHeader>
-                  <CardTitle>{inputPrompt.prompt}</CardTitle>
-                </CardHeader>
-                <Image
-                  alt="photo"
-                  src={src}
-                  width={512}
-                  height={512}
-                />
-              </Card>
-            ))}
-          </div>
-        </main>
-      </div>
+    <div>
+      <main className='flex flex-col items-center justify-center min-h-screen'>
+          <h1 className="font-bold text-5xl text-black pb-14">Generate <span className="text-blue-500">images</span> from text</h1>
+          <Image
+            src={homepageImage}
+            alt="Homepage Picture"
+            width={800}
+            height={200}
+          />
+          <Link href="/generate">
+            <Button className="text-xl mt-14 p-6 hover:bg-blue-500">Get Started</Button>
+          </Link>
+      </main>      
+    </div>
+      
     </>
   )
 }
