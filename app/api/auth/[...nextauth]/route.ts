c0d84caff6af6669da1820c256954facefa34a4f
Aniket Bindhani <aniketbindhani44@gmail.com>
2023-11-29 01:10:41 +0530

    Added session in backend so that no one can access the api routes without authorization

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,4 +11,4 @@ const authOptions = {
 }
 
 const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST, authOptions };
--- app/api/users/generate/route.ts
+++ app/api/users/generate/route.ts
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next"
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
@@ -6,14 +8,18 @@ const openai = new OpenAI({
 });
 
 export async function POST(request: NextRequest) {
+    const session = await getServerSession(authOptions)
     try {
+        if (!session) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
         const reqBody = await request.json();
         const {prompt} = reqBody;
 
         const res = await openai.images.generate({
             prompt: prompt,
             n:1,
-            size:"512x512"
+            size:"256x256"
         })
 
         console.log(res);
