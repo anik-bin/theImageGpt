8d802a23a9d379cb61b1ffc29f120fd9d55f91b9
Aniket Bindhani <aniketbindhani44@gmail.com>
2023-12-02 17:22:54 +0530

    Added Dark Mode. Not configured the styling yet

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { NextAuthProvider } from './Providers'
+import { ThemeProvider } from '@/components/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextAuthProvider>
-        {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
         </NextAuthProvider>
       </body>
     </html>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,18 @@
 "use client";
+import * as React from "react"
 import { useState } from 'react';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import { useSession, signOut } from 'next-auth/react';
 import Image from 'next/image';
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { useTheme } from "next-themes"
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { signIn } from 'next-auth/react'
@@ -24,6 +26,7 @@ const Navbar = () => {
   };
 
   const { status, data: session } = useSession();
+  const { setTheme } = useTheme();
 
   return (
     <nav className="bg-white p-4 pt-6">
@@ -39,19 +42,48 @@ const Navbar = () => {
 
         {status === 'authenticated' ?
           (
-            <div className={`lg:flex lg:space-x-4 ${isMenuOpen ? 'block' : 'hidden'}`}>
-              <DropdownMenu>
-                <DropdownMenuTrigger><Image src={session?.user?.image!} alt="Google account profile pic" width={40} height={40} className='rounded-full' /></DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuLabel>Name: <span>{session?.user?.name}</span></DropdownMenuLabel>
-                  <DropdownMenuLabel>Email: <span>{session?.user?.email}</span></DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem className="cursor-pointer" onClick={() => signOut()}>
-                    Log out
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
+
+            <>
+              <div className={`lg:flex lg:space-x-4 ${isMenuOpen ? 'block' : 'hidden'}`}>
+                <DropdownMenu>
+                  <DropdownMenuTrigger><Image src={session?.user?.image!} alt="Google account profile pic" width={40} height={40} className='rounded-full' /></DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>Name: <span>{session?.user?.name}</span></DropdownMenuLabel>
+                    <DropdownMenuLabel>Email: <span>{session?.user?.email}</span></DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className="cursor-pointer" onClick={() => signOut()}>
+                      Log out
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="icon">
+                      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                      <span className="sr-only">Toggle theme</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem onClick={() => setTheme("light")}>
+                      Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("dark")}>
+                      Dark
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("system")}>
+                      System
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+
+              
+            </>
+            
+
+            
 
           ) :
 
