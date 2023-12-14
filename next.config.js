8435cc3864b38349f6a69d0281d2f87c71457c14
Aniket Bindhani <aniketbindhani44@gmail.com>
2023-12-14 20:31:42 +0530

    updated nextconfig.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-          "googleusercontent.com",
-          "oaidalleapiprodscus.blob.core.windows.net",
-          "cdn.openai.com",
-        ]
-      },
+  images: {
+    domains: [
+      "googleusercontent.com",
+      "oaidalleapiprodscus.blob.core.windows.net",
+      "cdn.openai.com",
+    ]
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
